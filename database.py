dbe147e26f4f494cd5052a98d272c59558853e1a
RyanAquino <ryan.aquino7110@gmail.com>
2020-12-30 13:16:15 +0800

    ZDL-9: Refactor database.py, make database connection as decorator

--- database.py
+++ database.py
@@ -7,60 +7,58 @@ DB_USER = os.getenv("DB_USER") or "postgres"
 DB_HOST = os.getenv("DB_HOST") or "localhost"
 
 
-def verify_tables() -> bool:
+def db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = psycopg2.connect(
+            dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST
+        )
+        cur = conn.cursor()
+        value = func(cur, *args, **kwargs)
+        conn.commit()
+        conn.close()
+
+        return value
+
+    return wrapper
+
+
+@db_connection
+def verify_tables(cur) -> bool:
     """
     Check if products table already exist
     :return: boolean if product table exist
     """
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST
-    )
-    cur = conn.cursor()
     cur.execute(
         f"select exists(select * from information_schema.tables where table_name=%s)",
         ("products",),
     )
-    exist = cur.fetchone()[0]
-    conn.close()
 
-    return exist
+    return cur.fetchone()[0]
 
 
-def create_product_table() -> None:
+@db_connection
+def create_product_table(cur) -> None:
     """
     Create products table
     :return: None
     """
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST
-    )
-    cur = conn.cursor()
     cur.execute(
         "CREATE TABLE products (id SERIAL PRIMARY KEY, name VARCHAR(191), "
         "description VARCHAR(10485760), digital BOOLEAN default False, price FLOAT default 0, image VARCHAR, "
         "quantity INTEGER default 0, created_at timestamp default current_timestamp)"
     )
 
-    conn.commit()
-    conn.close()
 
-
-def save_product(data) -> None:
+@db_connection
+def save_product(cur, data) -> None:
     """
     Save product details into the database
+    :param cur: cursor
     :param data: Products model
     :return: None
     """
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST
-    )
-
-    cur = conn.cursor()
     cur.execute(
         "INSERT INTO products (name,description,price,image,quantity) "
         "VALUES(%(name)s, %(description)s, %(price)s, %(image)s, %(quantity)s)",
         data,
     )
-
-    conn.commit()
-    conn.close()
