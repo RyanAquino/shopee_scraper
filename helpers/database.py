8f2b4e08c0c677a7964484190b5ccda75588321b
RyanAquino <ryan.aquino7110@gmail.com>
2020-12-31 11:38:05 +0800

    ZDL-14: Add table name as environment variable

--- helpers/database.py
+++ helpers/database.py
@@ -1,10 +1,14 @@
 import psycopg2
 import os
+from dotenv import load_dotenv, find_dotenv
 
-DB_NAME = os.getenv("DB_NAME") or "shopee_scraper"
-DB_PASS = os.getenv("DB_PASS") or "1"
-DB_USER = os.getenv("DB_USER") or "postgres"
-DB_HOST = os.getenv("DB_HOST") or "localhost"
+load_dotenv(find_dotenv())
+
+DB_NAME = os.environ.get("DB_NAME") or "shopee_scraper"
+DB_PASS = os.environ.get("DB_PASS") or "1"
+DB_USER = os.environ.get("DB_USER") or "postgres"
+DB_HOST = os.environ.get("DB_HOST") or "localhost"
+DB_TABLE = os.environ.get("DB_TABLE") or "localhost"
 
 
 def db_connection(func):
@@ -30,7 +34,7 @@ def verify_tables(cur) -> bool:
     """
     cur.execute(
         f"select exists(select * from information_schema.tables where table_name=%s)",
-        ("products",),
+        (DB_TABLE,),
     )
 
     return cur.fetchone()[0]
@@ -43,7 +47,7 @@ def create_product_table(cur) -> None:
     :return: None
     """
     cur.execute(
-        "CREATE TABLE products (id SERIAL PRIMARY KEY, name VARCHAR(191), "
+        f"CREATE TABLE {DB_TABLE} (id SERIAL PRIMARY KEY, name VARCHAR(191), "
         "description VARCHAR(10485760), digital BOOLEAN default False, price FLOAT default 0, image VARCHAR, "
         "quantity INTEGER default 0, created_at timestamp default current_timestamp)"
     )
@@ -58,7 +62,7 @@ def save_product(cur, data) -> None:
     :return: None
     """
     cur.execute(
-        "INSERT INTO products (name,description,price,image,quantity) "
-        "VALUES(%(name)s, %(description)s, %(price)s, %(image)s, %(quantity)s)",
+        f"INSERT INTO {DB_TABLE} (name,description,price,image,quantity,created_at) "
+        "VALUES(%(name)s, %(description)s, %(price)s, %(image)s, %(quantity)s, %(created_at)s)",
         data,
     )
