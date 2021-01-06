89421090b41dd35878afea1f35db4e0878fbdbb1
RyanAquino <ryan.aquino7110@gmail.com>
2021-01-06 11:03:02 +0800

    ZDL-26: Update record when existing

--- helpers/database.py
+++ helpers/database.py
@@ -8,7 +8,7 @@ DB_NAME = os.environ.get("DB_NAME") or "shopee_scraper"
 DB_PASS = os.environ.get("DB_PASS") or "1"
 DB_USER = os.environ.get("DB_USER") or "postgres"
 DB_HOST = os.environ.get("DB_HOST") or "localhost"
-DB_TABLE = os.environ.get("DB_TABLE") or "localhost"
+DB_TABLE = os.environ.get("DB_TABLE") or "products_product"
 
 
 def db_connection(func):
@@ -47,7 +47,7 @@ def create_product_table(cur) -> None:
     :return: None
     """
     cur.execute(
-        f"CREATE TABLE {DB_TABLE} (id SERIAL PRIMARY KEY, name VARCHAR(191), "
+        f"CREATE TABLE {DB_TABLE} (id SERIAL PRIMARY KEY, name VARCHAR(10485760) UNIQUE, "
         "description VARCHAR(10485760), digital BOOLEAN default False, price FLOAT default 0, image VARCHAR, "
         "quantity INTEGER default 0, created_at timestamp default current_timestamp)"
     )
@@ -63,6 +63,8 @@ def save_product(cur, data) -> None:
     """
     cur.execute(
         f"INSERT INTO {DB_TABLE} (name,description,price,image,quantity,created_at) "
-        "VALUES(%(name)s, %(description)s, %(price)s, %(image)s, %(quantity)s, %(created_at)s)",
+        "VALUES(%(name)s, %(description)s, %(price)s, %(image)s, %(quantity)s, %(created_at)s) "
+        "ON CONFLICT (name) DO UPDATE SET (name, description, price, image, quantity, created_at) = "
+        "(EXCLUDED.name, EXCLUDED.description, EXCLUDED.price, EXCLUDED.image, EXCLUDED.quantity, EXCLUDED.created_at)",
         data,
     )
